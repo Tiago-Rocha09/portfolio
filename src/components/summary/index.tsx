322f8c63bf00b512c67a97dc8674b7c43a1b846d
Tiago Rocha <tiagoarrocha@gmail.com>
2023-09-25 14:36:40 -0300

    Ajuste seção resumo

--- src/components/summary/index.tsx
+++ src/components/summary/index.tsx
@@ -4,8 +4,8 @@ export const Summary = ({ dictionary }: Dictionary) => {
   const aboutDictionary = dictionary?.about || null;
 
   return (
-    <section className="bg-gray-750 w-full">
-      <ul className="relative sm:absolute sm:bottom-36 text-white block sm:grid grid-cols-2 p-5 left-1/2 -translate-x-1/2 bg-transparent sm:bg-gray-850 sm:rounded-[60px]">
+    <section className="bg-gray-750 w-full relative">
+      <ul className="relative sm:absolute sm:-bottom-16 text-white block sm:grid grid-cols-2 p-5 left-1/2 -translate-x-1/2 bg-transparent sm:bg-gray-850 sm:rounded-[60px]">
         <li className="p-8 text-center sm:border-r sm:border-r-white">
           <p className="text-4xl font-bold mb-2">
             {aboutDictionary?.yearsExperience || ""}
